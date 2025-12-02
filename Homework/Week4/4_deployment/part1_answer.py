2b5d9a46a34cdea92e4dae3d65759a3e278bc1af
hblovo <694186378@qq.com>
2025-12-03 05:57:10 +0800

    Init Homework 5/6

--- Homework/Week4/4_deployment/part1_answer.py
+++ Homework/Week4/4_deployment/part1_answer.py
@@ -1,5 +1,7 @@
 # TODO: Put your command to start mlserver in the variable below
 # command_to_start_mlserver = "your command as a string here"
 ### START CODE HERE
+a = 1
+b = 2
 command_to_start_mlserver = "mlserver start ."
 ### END CODE HERE
